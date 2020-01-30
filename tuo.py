7cc69ca67ec0fd519c419d10749ff4463be1e4a7
Tuoyuan Li <tuoyuanli@cs.uchicago.edu>
2020-01-30 16:01:36 -0600

    fasta

--- tuo.py
+++ tuo.py
@@ -5,5 +5,11 @@ def get_fasta(uniprot_id):
     uniprot_url = "https://www.uniprot.org/uniprot/" + uniprot_id + ".fasta"
     r = requests.get(uniprot_url)
     text = r.text.encode()
-    return text
+    soup = bs4.BeautifulSoup(text, "html5lib")
+    tags = soup.find_all("body")
+
+    return tags[0].text
+
+
+
 
