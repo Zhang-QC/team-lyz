517c37ae6109b0958e4e93f8d13235345602edf7
Zhang-QC <jasonzhangqc2000@gmail.com>
2020-02-01 14:16:29 -0600

    added protein class

--- util.py
+++ util.py
@@ -26,3 +26,39 @@ def get_uniprot_id(pdb_id):
 					return check_url[31:]
 	print("Your PDB ID is not valid.")
 	return None
+
+
+class protein:
+	def __init__(self, pdb_id = '', uniprot_id):
+		self.pdb_id = pdb_id
+		self.uniprot_id = uniprot_id
+		self.fasta = tuo.get_fasta(uniprot_id)
+		name, species, sequence = self.parse_fasta()
+		self.name = name
+		self.species = species
+		self.sequence = sequence
+		self.length = len(self.sequence)
+
+	def parse_fasta(self):
+		'''
+		Parse the FASTA file and determine the name, species, and sequence 
+		of the protein.
+		'''
+		name = ''
+		species = ''
+		sequence = ''
+		return name, species, sequence
+
+	def find_similar(self, len_diff, max_num, curated = True):
+		'''
+		Find similar proteins from the UniProt database using the assigned 
+		parameters.
+		'''
+		return None
+
+	def blast(self):
+		'''
+		Perform a BLAST search on the protein sequence.
+		'''
+		seq = self.sequence
+		return None
