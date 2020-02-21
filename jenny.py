813d380c54d52a1cc47192f3b4356bca6659674d
Ruilin Yang <ruilin@Ruilins-MacBook.local>
2020-02-20 22:21:51 -0600

    MSA

--- jenny.py
+++ jenny.py
@@ -17,6 +17,8 @@ import pa2util
 import urllib3
 import certifi
 
+from Bio.Alphabet import IUPAC, Gapped
+from Bio.Align import MultipleSeqAlignment
 
 
 
@@ -31,7 +33,7 @@ def find_nextpage(url):
     '''
     pm = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs = certifi.where())
     html = pm.urlopen(url=url, method='GET').data
-    soup = bs4.BeautifulSoup(html)
+    soup = bs4.BeautifulSoup(html,features="lxml")
     tags = soup.find_all("a", class_="nextPageLink")
     link = set()
     for tag in tags:
@@ -42,6 +44,14 @@ def find_nextpage(url):
 
 def find_uni_start(protein_name):
     return 'https://www.uniprot.org/uniprot/?query=' + protein_name +'&sort=score'
+
+#prefer to have dictionary with id as key and alignment as value
+def create_MSA(alignment_list):
+    align = MultipleSeqAlignment([], Gapped(IUPAC.unambiguous_dna, "-"))
+    for id_ in alignment_list.keys():
+         align.add_sequence(id_, alignment_list[id_])
+    print(align)
+
 
 
   
