8d3d9db3572b36531dfcba72b54273496270968c
Tuoyuan Li <tuoyuanli@cs.uchicago.edu>
2020-02-27 15:47:39 -0600

    com

--- lyzsite/lyz/views.py
+++ lyzsite/lyz/views.py
@@ -1,7 +1,135 @@
+import json
+import traceback
+import sys
+import csv
+import os
+import re
+from functools import reduce
+from operator import and_
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django import forms
 
-# Create your views here.
+from util_test import get_fasta
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the lyz index.")
+NOPREF_STR = 'No preference'
+RES_DIR = os.path.join(os.path.dirname(__file__), '..', 'res')
+COLUMN_NAMES = dict(
+    dept='Deptartment',
+    course_num='Course',
+    section_num='Section',
+    day='Day',
+    time_start='Time (start)',
+    time_end='Time (end)',
+    enroll='Enrollment',
+)
+
+
+def _valid_result(pdb):
+    '''
+    Validate results returned by find_courses.
+
+    (HEADER, RESULTS) = [0, 1]
+    ok = (isinstance(res, (tuple, list)) and
+          len(res) == 2 and
+          isinstance(res[HEADER], (tuple, list)) and
+          isinstance(res[RESULTS], (tuple, list)))
+    if not ok:
+        return False
+
+    n = len(res[HEADER])
+
+    def _valid_row(row):
+        return isinstance(row, (tuple, list)) and len(row) == n
+    return reduce(and_, (_valid_row(x) for x in res[RESULTS]), True)
+    '''
+
+    if re.search("[A-Z0-9]{4}",res) != None and len(res) == 4:
+        return True
+    return False
+
+
+'''
+
+def _valid_military_time(time):
+    return (0 <= time < 2400) and (time % 100 < 60)
+
+
+def _load_column(filename, col=0):
+    """Load single column from csv file."""
+    with open(filename) as f:
+        col = list(zip(*csv.reader(f)))[0]
+        return list(col)
+
+
+def _load_res_column(filename, col=0):
+    """Load column from resource directory."""
+    return _load_column(os.path.join(RES_DIR, filename), col=col)
+
+
+def _build_dropdown(options):
+    """Convert a list to (value, caption) tuples."""
+    return [(x, x) if x is not None else ('', NOPREF_STR) for x in options]
+
+'''
+
+
+
+class SearchForm(forms.Form):
+    query = forms.CharField(
+        label='Search Protein',
+        help_text='e.g. 1J6Z',
+        required=True)
+
+
+
+def home(request):
+    context = {}
+    res = None
+    if request.method == 'GET':
+        # create a form instance and populate it with data from the request:
+        form = SearchForm(request.GET)
+        # check whether it's valid:
+        if form.is_valid():
+            # Convert form data to an args dictionary for find_courses
+            args = {}
+            if form.cleaned_data['query']:
+                args['terms'] = form.cleaned_data['query']
+            try:
+                res = get_fasta(args)
+            except Exception as e:
+                print('Exception caught')
+                bt = traceback.format_exception(*sys.exc_info()[:3])
+                context['err'] = """
+                An exception was thrown in:
+                <pre>{}
+{}</pre>
+                """.format(e, '\n'.join(bt))
+
+                res = None
+    else:
+        form = SearchForm()
+
+    # Handle different responses of res
+    if res is None:
+        context['result'] = None
+    elif isinstance(res, str):
+        context['result'] = None
+        context['err'] = res
+        result = None
+    elif not _valid_result(res):
+        context['result'] = None
+        context['err'] = ('Return of XXX has the wrong data type.')
+    else:
+        columns, result = res
+
+        # Wrap in tuple if result is not already
+        if result and isinstance(result[0], str):
+            result = [(r,) for r in result]
+
+        context['result'] = result
+        context['num_results'] = len(result)
+        context['columns'] = [COLUMN_NAMES.get(col, col) for col in columns]
+
+    context['form'] = form
+    return render(request, 'index.html', context)
